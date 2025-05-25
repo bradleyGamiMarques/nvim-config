404a7533b4d01eb6e1a4bb74505bacc78826f6ae
Bradley Marques <bradley.marques@evolus.com>
2025-05-25 13:43:04 -0700

    Feat: add synthweave theme
    - Add dotfile support

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    -- Toggle the filesystem view with Ctrl+n (on the right)
+    -- Keymaps
     vim.keymap.set('n', '<leader>n', function()
       require('neo-tree.command').execute({
         toggle = true,
@@ -17,7 +17,6 @@ return {
       })
     end, { desc = "Toggle Neo-tree filesystem (right)" })
 
-    -- Focus the filesystem if it's open
     vim.keymap.set('n', '<leader>e', function()
       require('neo-tree.command').execute({
         action = "focus",
@@ -26,13 +25,44 @@ return {
       })
     end, { desc = "Focus Neo-tree filesystem (right)" })
 
-    -- Close the filesystem
     vim.keymap.set('n', '<leader>c', function()
       require('neo-tree.command').execute({
         action = "close",
         source = "filesystem",
         position = "right",
       })
-    end, { desc = "Close Neo-tree filesystem (right)" })end,
+    end, { desc = "Close Neo-tree filesystem (right)" })
+    -- Configure Neo-tree
+    require('neo-tree').setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,          -- Show hidden files (dotfiles)
+          show_hidden_count = true,
+          hide_dotfiles = false,   -- Do not hide dotfiles
+          hide_gitignored = false, -- Show gitignored files
+        },
+      },
+    })
+
+    -- Auto-apply Synthwave84 color highlights
+    local function apply_synthwave84_highlights()
+      vim.cmd [[
+        highlight NeoTreeDirectoryName guifg=#f92aad
+        highlight NeoTreeFileName guifg=#fafff9
+        highlight NeoTreeFileNameOpened guifg=#fdf200 gui=bold
+        highlight NeoTreeGitModified guifg=#ff8b39
+        highlight NeoTreeGitAdded guifg=#00ffcc
+        highlight NeoTreeGitDeleted guifg=#ff005e
+        highlight NeoTreeIndentMarker guifg=#7e57c2
+        ]]
+    end
+
+    -- Apply now and whenever the colorscheme changes
+    apply_synthwave84_highlights()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = apply_synthwave84_highlights,
+    })
+  end,
 }
 
